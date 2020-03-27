e1b2b96892e8ae49fdb8d0144593978f1cf5eb75
Ben Lambert <ben.c.lambert@gmail.com>
2020-03-27 00:13:23 +0000

    added in plot of r star by quantile for 8 schools example

--- documentation/plan.md
+++ documentation/plan.md
@@ -1,6 +1,8 @@
 * Add visualisation of R* by quantile of each parameter:
-  * Done for eight schools $\tau$ parameter
-  * [ ] Add function that can plot this for a given parameter
+  * [x] Done for eight schools $\tau$ and $\mu$ parameters
+
+  * [x] Add function that can plot this for a given parameter
+
 * Separate out examples, each into its own file, using common r_star and r_star_distribution files
   * [x] Eight schools
   * [ ] Ar1
@@ -8,9 +10,17 @@
   * [ ] Wide datasets
   * [x] Trends
   * [ ] Cauchy
+
 * For some datasets, include how far the distributions are from the truth:
   * Plot of $R^*$ vs KL divergence: doesn't work for Cauchy because the tails are so poorly estimated, but can try with normal case
   * Plot of Anderson-Darling test statistic seems ok for Cauchy and is based on differences in eCDF (but is better than KS statistic which is biased towards the middle of a distribution)
   * Same but with $\hat{R}$
-  * [ ] Include plot of Cauchy_convergence which plots R^2, R* and Rhat each verses distribution
-* $R^*$ vs iterations for each of the problems (where there is a known truth)
+  * [x] Include plot of Cauchy_convergence which plots R^2, R* and Rhat each verses distribution
+
+* $R^*$ vs iterations for each of the problems (where there is a known truth):
+
+  * [x] Done for Cauchy problem
+
+* Change colours to monochrome:
+  * [ ] Cauchy convergence
+  * [ ] MVT normal
--- src/r_star_monitor.R
+++ src/r_star_monitor.R
@@ -222,3 +222,146 @@ r_hat_multivariate <- function(x, split_chains=T){
   R_hat <- (nchains + 1) / nchains * max(abs(y$values)) + (niter - 1) / niter
   return(R_hat)
 }
+
+#' Plot R* versus parameter quantiles
+#'
+#' @param index index of parameter in x
+#' 
+#' @param x a 3D array of samples of dimensions: # iter x # chains x # parameters
+#' 
+#' @param split_chains a Boolean (defaults to true) indicating whether to split
+#' chains into two equal halves
+#'
+#' @param training_percent proportion of iterations used to train GBM model
+#' (default is 0.7)
+#'
+#' @param caret_default GBM hyperparameter settings given as a list (defaults
+#' are interaction.depth=3, n.trees=50, shrinkage=0.1, nminobsinmode=10)
+#'
+#' @param nsim number of samples of R* to generate from GBM fit (defaults to
+#' 20)
+#'
+#' @return distribution of R* values
+#' 
+#' @references Ben Lambert, Aki Vehtari (2020) R*: A robust MCMC convergence
+#' diagnostic with uncertainty using gradient-boosted machines
+#' \emph{arXiv preprint} \code{arXiv:TBD}
+plot_r_star_quantiles <- function(index, x, split_chains=T, training_percent=0.7, caret_default=NA, nsim=20){
+  if(split_chains)
+    x <- split_data(x)
+  
+  nparams <- dim(x)[3]
+  nchains <- dim(x)[2]
+  niter <- dim(x)[1]
+  m_flattened <- matrix(nrow = nrow(x) * nchains,
+                        ncol = (nparams + 1))
+  k <- 1
+  for(i in 1:nchains){
+    for(j in 1:nrow(x)){
+      m_flattened[k, 1:nparams] <- x[j, i, ]
+      m_flattened[k, (nparams + 1)] <- i
+      k <- k + 1
+    }
+  }
+  m_flattened <- m_flattened %>% 
+    as.data.frame()
+  colnames(m_flattened)[nparams + 1] <- "chain"
+  r <- m_flattened %>% 
+    mutate(chain=as.factor(chain))
+  
+  rand_samples <- sample(1:nrow(r), training_percent * nrow(r))
+  training_data <- r[rand_samples, ]
+  testing_data <- r[-rand_samples, ]
+  
+  if(is.na(caret_default))
+    caretGrid <- expand.grid(interaction.depth=c(3), 
+                             n.trees = 50,
+                             shrinkage=c(0.1),
+                             n.minobsinnode=10)
+  else
+    caretGrid <- expand.grid(caret_grid_default)
+  
+  gbmFit1 <- train(chain ~ ., data = training_data, 
+                   method = "gbm",
+                   trControl = trainControl(method = 'none'), 
+                   tuneGrid = caretGrid, verbose=FALSE)
+  plda <- predict(object=gbmFit1, newdata=testing_data, type = "prob")
+  
+  mAccuracy <- matrix(nrow = nrow(plda),
+                      ncol = nsim)
+  for(j in 1:nrow(plda)){
+    test <- apply(rmultinom(nsim, 1, prob = plda[j, ]), 2, function(x) which(x==1))
+    mAccuracy[j, ] <- if_else(test==testing_data$chain[j], 1, 0)
+  }
+  mAccuracy <- as.data.frame(mAccuracy)
+  mAccuracy$param <- testing_data[, index]
+  
+  lbreaks <- quantile(mAccuracy$param, seq(0, 1, 0.1))
+  mAccuracy$param_cuts <- cut(mAccuracy$param, breaks=lbreaks, include.lowest = T, right=T, ordered_result = T)
+  cuts <- sort(unique(mAccuracy$param_cuts))
+  r_star <- matrix(ncol = length(cuts),
+                   nrow=nsim)
+  for(i in seq_along(cuts)){
+    temp <- mAccuracy %>% 
+      filter(param_cuts==cuts[i])
+    r_star[, i] <- colMeans(temp[, 1:nsim]) * n_distinct(testing_data$chain)
+  }
+  colnames(r_star) <- seq(0.1, 1, 0.1)
+  r_mean <- 
+    r_star %>% 
+    melt(id.vars=NULL) %>%  
+    group_by(Var2) %>% 
+    summarise(value=mean(value))
+  r_full <- 
+    r_star %>%
+    melt(id.vars=NULL) %>%
+    bind_rows(r_mean) %>% 
+    mutate(variable=as.numeric(as.character(Var2))) 
+  
+  ggplot(filter(r_full, !is.na(Var1)),
+         aes(x=variable, y=value)) +
+    geom_jitter(height = 0.02, width = 0.02, colour="grey") +
+    ylab(TeX("$R*$")) +
+    xlab("Quantile") +
+    geom_hline(yintercept = 1, linetype=2) +
+    theme(text = element_text(colour="black", size=11),
+          axis.text = element_text(colour="black", size=10)) +
+    geom_line(data=filter(r_full, is.na(Var1))) +
+    xlim(0, 1.01)
+}
+
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
